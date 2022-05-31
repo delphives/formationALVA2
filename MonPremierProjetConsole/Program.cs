ccca87170d9e4fed93cd9b85656092ef76b84961
Yves-Collet <Yves.Collet@loro.ch>
2022-05-31 10:00:39 +0200

    première version

---
+++ MonPremierProjetConsole/Program.cs
@@ -0,0 +1,63 @@
+﻿//public static void Main(string[] args)
+//{
+
+// See https://aka.ms/new-console-template for more information
+//commentaire une ligne
+//ici on se trouve à l'intérieur de la méthode Maint
+
+
+/*sdfsdf
+ * 
+ * 
+ * commentaires sur plusieurs lignes
+ * 
+ * 
+ */
+
+
+using MonNamespace;
+/** 
+* commentaire pour les méthodes, ....
+*/
+Console.WriteLine("Hello, World!");
+string? toto = "";
+toto = null;
+toto = "asdf";
+int? tutu;
+tutu = null;
+int? age = null;
+//string? x = Console.ReadLine();
+Object y = null;
+tutu = 2147483647;
+age = 175765;
+tutu += 2147483647 + age;
+
+//if
+if ((false) || (true && false) || true)
+{
+
+}
+
+if (age > 2)
+{
+    Console.WriteLine("+");
+    Console.WriteLine("toot");
+}
+
+if (age > 2)
+    Console.WriteLine("toot");
+Console.WriteLine("+");
+
+
+//if simplifié
+string z = "";
+if (age > 2)
+    z = "+";
+else
+    z = "-";
+z = age > 2 ? "+" : "-";
+
+Maths m = new Maths();
+Console.WriteLine(m.CalculerSommeDeDeuxNombres(4, 6));
+Console.ReadLine();
+//}
---
+++ MonPremierProjetConsole/utilitaires/Maths.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonNamespace
+{
+    /*visibilités :
+     * public --> tout le monde peut voir
+     * private --> seule la classe contenant l'élément privé peut le voir
+     * protected --> la classe en cours + les classes enfants ou qui héritent
+     * internal --> seulement les classes du même namespace*/
+    /// <summary>
+    /// classe Maths pour des opérations mathématiques complexes
+    /// </summary>
+    public class Maths
+    {
+ 
+        /// <summary>
+        /// méthode qui cacule la somme de 2 nombres
+        /// </summary>
+        /// <param name="nb1">le nombre numéro 1</param>
+        /// <param name="nb2">le nombre 2</param>
+        /// <returns>la somme</returns>
+        public int CalculerSommeDeDeuxNombres(int nb1, int nb2)
+        {
+            return nb1 + nb2;
+        }
+    }
+}
